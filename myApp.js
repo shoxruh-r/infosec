94de8d2c4b0bac41ee30136b83f8184ba5c19c7f
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-06-11 21:15:23 +0500

    cache

--- myApp.js
+++ myApp.js
@@ -10,7 +10,7 @@ app.use(helmet.noSniff())
 app.use(helmet.ieNoOpen())
 app.use(helmet.hsts({ maxAge: 90 * 24 * 60 * 60, force: true }))
 app.use(helmet.dnsPrefetchControl())
-
+app.use(helmet.noCache())
 
 
 
