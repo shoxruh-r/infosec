52a36c13bc2dcde1ca557c5f72802a23906b6022
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-06-11 21:12:23 +0500

    dns

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.use(helmet.xssFilter())
 app.use(helmet.noSniff())
 app.use(helmet.ieNoOpen())
 app.use(helmet.hsts({ maxAge: 90 * 24 * 60 * 60, force: true }))
-
+app.use(helmet.dnsPrefetchControl())
 
 
 
