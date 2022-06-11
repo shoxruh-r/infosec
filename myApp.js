d981377e497b523107e40c59aad9c25f2e06202e
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-06-11 20:30:42 +0500

    SSL

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ app.use(helmet.frameguard({ action: 'deny' }))
 app.use(helmet.xssFilter())
 app.use(helmet.noSniff())
 app.use(helmet.ieNoOpen())
-
+app.use(helmet.hsts({ maxAge: 90 * 24 * 60 * 60, force: true }))
 
 
 
